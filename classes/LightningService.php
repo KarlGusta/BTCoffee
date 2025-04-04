675b2be9b65ad97a5ef2e28eefc80b8eba338b5c
Esimit Karlgusta <annoh.karlgusta@gmail.com>
2025-04-04 13:01:49 +0300

    Updated the LightningService Constructor to use the config file

--- classes/LightningService.php
+++ classes/LightningService.php
@@ -5,9 +5,10 @@ class LightningService {
 
     public function __construct() {
         // Load configuration from config file
+        $config = include_once __DIR__ . '/../config/lightning_config.php';
         // In production, these should be stored securely
-        $this->api_key = getenv('LIGHTNING_API_KEY') ?: 'your_opennode_api_key';
-        $this->api_url = getenv('LIGHTNING_API_URL') ?: 'https://api.opennode.com/v1/';
+        $this->api_key = $config['api_key'];
+        $this->api_url = $config['api_url'];
     }
 
     /**
