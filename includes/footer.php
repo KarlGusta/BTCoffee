c54ded0f3f5d874b3af4b57ed59d1a1839f483ae
Esimit Karlgusta <annoh.karlgusta@gmail.com>
2025-04-07 18:11:18 +0300

    Added more initial design files

---
+++ includes/footer.php
@@ -0,0 +1,32 @@
+<footer class="footer footer-transparent d-print-none">
+  <div class="container-xl">
+    <div class="row text-center align-items-center flex-row-reverse">
+      <div class="col-12 col-lg-auto mt-3 mt-lg-0">
+        <ul class="list-inline list-inline-dots mb-0 footer-text-small">
+          <li class="list-inline-item">
+            Copyright &copy; <script>
+              document.write(new Date().getFullYear())
+            </script>
+            <a href="." class="link-secondary">BTCoffee</a>.
+            All rights reserved.
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
+</div>
+</div>
+<!-- Libs JS -->
+<script src="<?php echo path('assets', 'dist'); ?>libs/apexcharts/dist/apexcharts.min.js" defer></script>
+<!-- Datepicker -->
+<script src="<?php echo path('assets', 'dist'); ?>libs/litepicker/dist/litepicker.js" defer></script>
+<script src="<?php echo path('assets', 'dist'); ?>libs/jsvectormap/dist/js/jsvectormap.min.js" defer></script>
+<script src="<?php echo path('assets', 'dist'); ?>libs/jsvectormap/dist/maps/world.js" defer></script>
+<script src="<?php echo path('assets', 'dist'); ?>libs/jsvectormap/dist/maps/world-merc.js" defer></script>
+<!-- Tabler Core -->
+<script src="<?php echo path('assets', 'dist'); ?>js/tabler.min.js" defer></script>
+<script src="<?php echo path('assets', 'dist'); ?>js/demo.min.js" defer></script>
+</body>
+
+</html>
