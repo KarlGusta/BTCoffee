9c6ff1fc9cfc7ea7712fd40029aa08c2a113965c
Esimit Karlgusta <annoh.karlgusta@gmail.com>
2025-04-09 10:57:45 +0300

    Added the form

--- views/creator/creator.php
+++ views/creator/creator.php
@@ -6,44 +6,83 @@ include '../../includes/header.php';
 ?>
 
 <div class="page-wrapper">
-        <div class="container-xl">
-          <!-- Page title -->
-          <div class="page-header d-print-none">
-            <div class="row g-2 align-items-center">
-              <!-- Empty for spacing purposes -->
+  <div class="container-xl">
+    <!-- Page title -->
+    <div class="page-header d-print-none">
+      <div class="row g-2 align-items-center">
+        <!-- Empty for spacing purposes -->
+      </div>
+    </div>
+  </div>
+  <div class="page-body">
+    <div class="container-xl">
+      <div class="row row-deck row-cards">
+        <div class="col-lg-6">
+          <div class="card">
+            <div class="card-body">
+              <h3 class="card-title">Traffic summary</h3>
+              <div id="chart-mentions" class="chart-lg"></div>
             </div>
           </div>
         </div>
-        <div class="page-body">
-          <div class="container-xl">
-            <div class="row row-deck row-cards">
-              <div class="col-12">
-                <div class="card card-md">
-                  <div class="card-stamp card-stamp-lg">
-                    <div class="card-stamp-icon bg-primary">
-                      <!-- Download SVG icon from http://tabler-icons.io/i/ghost -->
-                      <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 11a7 7 0 0 1 14 0v7a1.78 1.78 0 0 1 -3.1 1.4a1.65 1.65 0 0 0 -2.6 0a1.65 1.65 0 0 1 -2.6 0a1.65 1.65 0 0 0 -2.6 0a1.78 1.78 0 0 1 -3.1 -1.4v-7" /><line x1="10" y1="10" x2="10.01" y2="10" /><line x1="14" y1="10" x2="14.01" y2="10" /><path d="M10 14a3.5 3.5 0 0 0 4 0" /></svg>
-                    </div>
-                  </div>
-                  <div class="card-body">
-                    <div class="row align-items-center">
-                      <div class="col-10">
-                        <h3 class="h1">Tabler Icons</h3>
-                        <div class="markdown text-muted">
-                          All icons come from the Tabler Icons set and are MIT-licensed. Visit
-                          <a href="https://tabler-icons.io" target="_blank" rel="noopener">tabler-icons.io</a>,
-                          download any of the 1960 icons in SVG, PNG or&nbsp;React and use them in your favourite design tools.
-                        </div>
-                        <div class="mt-3">
-                          <a href="https://tabler-icons.io" class="btn btn-primary" target="_blank" rel="noopener">Download icons</a>
-                        </div>
-                      </div>
-                    </div>
+        <div class="col-lg-6">
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">Basic form</h3>
+            </div>
+            <div class="card-body">
+              <form>
+                <div class="form-group mb-3 ">
+                  <label class="form-label">Email address</label>
+                  <div>
+                    <input type="email" class="form-control" aria-describedby="emailHelp" placeholder="Enter email">
+                    <small class="form-hint">We'll never share your email with anyone else.</small>
                   </div>
                 </div>
-              </div>
+                <div class="form-group mb-3 ">
+                  <label class="form-label">Password</label>
+                  <div>
+                    <input type="password" class="form-control" placeholder="Password">
+                    <small class="form-hint">
+                      Your password must be 8-20 characters long, contain letters and numbers, and must not contain
+                      spaces, special characters, or emoji.
+                    </small>
+                  </div>
+                </div>
+                <div class="form-group mb-3 ">
+                  <label class="form-label">Select</label>
+                  <div>
+                    <select class="form-select">
+                      <option>Option 1</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="form-group mb-3">
+                  <label class="form-label">Checkboxes</label>
+                  <div>
+                    <label class="form-check">
+                      <input class="form-check-input" type="checkbox" checked="">
+                      <span class="form-check-label">Option 1</span>
+                    </label>
+                    <label class="form-check">
+                      <input class="form-check-input" type="checkbox">
+                      <span class="form-check-label">Option 2</span>
+                    </label>
+                    <label class="form-check">
+                      <input class="form-check-input" type="checkbox" disabled="">
+                      <span class="form-check-label">Option 3</span>
+                    </label>
+                  </div>
+                </div>
+                <div class="form-footer">
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
+      </div>
+    </div>
+  </div>
 
-<?php include '../../includes/footer.php'; ?>
+  <?php include '../../includes/footer.php'; ?>
