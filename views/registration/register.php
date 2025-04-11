ba7c8de0f0772263c8baf546abefe31bfa6d2e27
Esimit Karlgusta <annoh.karlgusta@gmail.com>
2025-04-11 13:58:28 +0300

    Removed unnecessary inputs

--- views/registration/register.php
+++ views/registration/register.php
@@ -46,55 +46,23 @@ include __DIR__ . '/../../config/paths.php';
             <div class="text-center mb-4">
                 <a href="." class="navbar-brand navbar-brand-autodark"><img src="./static/logo.svg" height="36" alt=""></a>
             </div>
-            <form class="card card-md" action="." method="get">
+            <form class="card card-md card-custom" action="." method="get">
                 <div class="card-body">
-                    <h2 class="card-title text-center mb-4">Create your account</h2>
+                    <h2 class="card-title text-center mb-4"><strong>Create your account</strong></h2>
                     <div class="mb-3">
                         <label class="form-label">Choose a username for your page</label>
                         <div class="input-group input-group-flat">
                             <span class="input-group-text">
                                 <strong>https://btcoffee.com/</strong>
                             </span>
-                            <input type="text" class="form-control ps-0 text-muted" value="yourname" autocomplete="off">
+                            <input type="text" class="form-control ps-0 text-muted is-valid is-valid-lite" value="yourname" autocomplete="off">
                         </div>
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label">Name</label>
-                        <input type="text" class="form-control" placeholder="Enter name">
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label">Email address</label>
-                        <input type="email" class="form-control" placeholder="Enter email">
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label">Password</label>
-                        <div class="input-group input-group-flat">
-                            <input type="password" class="form-control" placeholder="Password" autocomplete="off">
-                            <span class="input-group-text">
-                                <a href="#" class="link-secondary" title="Show password" data-bs-toggle="tooltip"><!-- Download SVG icon from http://tabler-icons.io/i/eye -->
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                        <circle cx="12" cy="12" r="2" />
-                                        <path d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7" />
-                                    </svg>
-                                </a>
-                            </span>
-                        </div>
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-check">
-                            <input type="checkbox" class="form-check-input" />
-                            <span class="form-check-label">Agree the <a href="./terms-of-service.html" tabindex="-1">terms and policy</a>.</span>
-                        </label>
                     </div>
                     <div class="form-footer">
-                        <button type="submit" class="btn btn-primary w-100">Create new account</button>
+                        <button type="submit" class="btn btn-primary w-100 btn-custom">Sign Up</button>
                     </div>
                 </div>
             </form>
-            <div class="text-center text-muted mt-3">
-                Already have account? <a href="./sign-in.html" tabindex="-1">Sign in</a>
-            </div>
         </div>
     </div>
     <!-- Libs JS -->
