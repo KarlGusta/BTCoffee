a8ee8a83de7d63ea96c32ba34797a90a691c28f8
Esimit Karlgusta <annoh.karlgusta@gmail.com>
2025-05-15 17:20:22 +0300

    Added more styling

--- includes/admin/admin_header.php
+++ includes/admin/admin_header.php
@@ -216,7 +216,7 @@ include __DIR__ . '/../../config/paths.php';
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="./index.html">
+                            <a class="nav-link" href="payout-settings.php">
                                 <span class="nav-link-icon d-md-none d-lg-inline-block">
                                     <!-- Money/Cash icon -->
                                     <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
