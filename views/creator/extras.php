bad3417f8e74545b696bc472e62086949e9150e0
Esimit Karlgusta <annoh.karlgusta@gmail.com>
2025-05-19 15:11:15 +0300

    Added respective icons

--- views/creator/extras.php
+++ views/creator/extras.php
@@ -24,48 +24,88 @@ include '../../includes/admin/admin_header.php';
                     </div>
                 </div>
                 <div class="card card-custom">
-                    <div class="card-body text-center py-4 p-sm-5">
+                    <div class="card-body text-center py-4">
                         <h1 class="mt-1">Add a new listing</h1>
                         <p class="text-muted">Pick a template or start from scratch</p>
                         <div class="row row-deck text-center">
                             <div class="col-sm-6 col-lg-4 mb-3">
-                                <div class="card" style="background-color: #F4F4F4;">
+                                <div class="card card-custom" style="background-color: #F4F4F4;">
                                     <div class="card-body">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icons-tabler-outline icon-tabler-file-plus" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                            <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+                                            <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
+                                            <path d="M12 11v6" />
+                                            <path d="M9 14h6" />
+                                        </svg>
                                         <p class="text-muted">Start from scratch</p>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-sm-6 col-lg-4 mb-3">
-                                <div class="card" style="background-color: #F3FAF4;">
+                                <div class="card card-custom" style="background-color: #F3FAF4;">
                                     <div class="card-body">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icons-tabler-outline icon-tabler-file-download" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                            <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+                                            <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
+                                            <path d="M12 17v-6" />
+                                            <path d="M9.5 14.5l2.5 2.5l2.5 -2.5" />
+                                        </svg>
                                         <p class="text-muted">Digital product</p>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-sm-6 col-lg-4 mb-3">
-                                <div class="card" style="background-color: #FFFEF3;">
+                                <div class="card card-custom" style="background-color: #FFFEF3;">
                                     <div class="card-body">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icons-tabler-outline icon-tabler-brand-instagram" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                            <path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z" />
+                                            <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+                                            <path d="M16.5 7.5l0 .01" />
+                                        </svg>
                                         <p class="text-muted">Instagram close friends access</p>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-sm-6 col-lg-4 mb-3">
-                                <div class="card" style="background-color: #F9F2FA;">
+                                <div class="card card-custom" style="background-color: #F9F2FA;">
                                     <div class="card-body">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icons-tabler-outline icon-tabler-video" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                            <path d="M15 10l4.553 -2.276a1 1 0 0 1 1.447 .894v6.764a1 1 0 0 1 -1.447 .894l-4.553 -2.276v-4z" />
+                                            <path d="M3 6m0 2a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2z" />
+                                        </svg>
                                         <p class="text-muted">1-on-1 Zoom call</p>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-sm-6 col-lg-4 mb-3">
-                                <div class="card" style="background-color: #E8EAF6;">
+                                <div class="card card-custom" style="background-color: #E8EAF6;">
                                     <div class="card-body">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icons-tabler-outline icon-tabler-ticket" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                            <path d="M15 5l0 2" />
+                                            <path d="M15 11l0 2" />
+                                            <path d="M15 17l0 2" />
+                                            <path d="M5 5h14a2 2 0 0 1 2 2v3a2 2 0 0 0 0 4v3a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-3a2 2 0 0 0 0 -4v-3a2 2 0 0 1 2 -2" />
+                                        </svg>
                                         <p class="text-muted">Ticket for an event</p>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-sm-6 col-lg-4 mb-3">
-                                <div class="card" style="background-color: #E3F2FD;">
+                                <div class="card card-custom" style="background-color: #E3F2FD;">
                                     <div class="card-body">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icons-tabler-outline icon-tabler-package" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                            <path d="M12 3l8 4.5l0 9l-8 4.5l-8 -4.5l0 -9l8 -4.5" />
+                                            <path d="M12 12l8 -4.5" />
+                                            <path d="M12 12l0 9" />
+                                            <path d="M12 12l-8 -4.5" />
+                                            <path d="M16 5.25l-8 4.5" />
+                                        </svg>
                                         <p class="text-muted">Physical good</p>
                                     </div>
                                 </div>
