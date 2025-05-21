d3ca794531f42e662f31e7af8c1a40ac50694fbc
Esimit Karlgusta <annoh.karlgusta@gmail.com>
2025-05-21 12:30:21 +0300

    Added more styling

--- views/creator/new-listing.php
+++ views/creator/new-listing.php
@@ -4,33 +4,90 @@ ini_set('display_errors', 1);
 
 // Include database, creator class, and paths configuration
 include_once '../../config/paths.php';
-
-// Only include header files after all potential redirects
-include '../../includes/admin/admin-header-links.php';
-include '../../includes/admin/admin-header.php';
 ?>
-<div class="page-body">
-    <div class="container-xl">
+<!doctype html>
+<html lang="en">
 
-        <div class="row row-cards">
-            <div class="col-md-8 mx-auto">
-                <div class="card card-custom">
-                    <div class="card-body text-center py-4 p-sm-5">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-building-store">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>BTCoffee - Easiest way to tip with Bitcoin</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="<?php echo path('assets', 'img'); ?>favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="<?php echo path('assets', 'img'); ?>favicon.ico" type="image/x-icon">
+
+    <!-- CSS files - Updated with path() function -->
+    <link href="<?php echo path('assets', 'dist'); ?>css/tabler.min.css" rel="stylesheet" />
+    <link href="<?php echo path('assets', 'dist'); ?>css/tabler-flags.min.css" rel="stylesheet" />
+    <link href="<?php echo path('assets', 'dist'); ?>css/tabler-payments.min.css" rel="stylesheet" />
+    <link href="<?php echo path('assets', 'dist'); ?>css/tabler-vendors.min.css" rel="stylesheet" />
+    <link href="<?php echo path('assets', 'dist'); ?>css/demo.min.css" rel="stylesheet" />
+
+    <!-- Custom CSS -->
+    <link href="<?php echo path('assets', 'css'); ?>style.css" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="page">
+        <header class="navbar navbar-expand-md navbar-light d-print-none">
+            <div class="container-xl">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-menu">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <h1 class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
+                    <a href="https://github.com/tabler/tabler" class="btn" target="_blank" rel="noreferrer">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-left">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M3 21l18 0" />
-                            <path d="M3 7v1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1h-18l2 -4h14l2 4" />
-                            <path d="M5 21l0 -10.15" />
-                            <path d="M19 21l0 -10.15" />
-                            <path d="M9 21v-4a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v4" />
+                            <path d="M15 6l-6 6l6 6" />
                         </svg>
-                        <h1 class="mt-1">You haven't added anything yet.</h1>
-                        <p class="text-muted">Shop is a simple and effective way to offer something to your audience. It could be anything. See some examples here, here, and here</p>
+                    </a>
+                </h1>
+                <h2>What are you offering?</h2>
+                <div class="navbar-nav flex-row order-md-last">
+                    <div class="nav-item d-none d-md-flex me-3">
+                        <div class="btn-list">
+                            <a href="https://github.com/tabler/tabler" class="btn" target="_blank" rel="noreferrer">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <circle cx="12" cy="12" r="2" />
+                                    <path d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7" />
+                                </svg>
+                            </a>
+                            <a href="https://github.com/tabler/tabler" class="btn btn-light" target="_blank" rel="noreferrer">
+                                Save as draft
+                            </a>
+                            <a href="https://github.com/sponsors/codecalm" class="btn btn-orange" target="_blank" rel="noreferrer">
+                                Publish
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-</div>
+        </header>
+        <div class="page-wrapper">
+            <div class="page-body">
+                <div class="container-xl">
+                    <div class="row row-cards">
+                        <div class="col-md-8 mx-auto">
+                            <div class="card card-custom">
+                                <div class="card-body text-center py-4 p-sm-5">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-building-store">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                        <path d="M3 21l18 0" />
+                                        <path d="M3 7v1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1h-18l2 -4h14l2 4" />
+                                        <path d="M5 21l0 -10.15" />
+                                        <path d="M19 21l0 -10.15" />
+                                        <path d="M9 21v-4a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v4" />
+                                    </svg>
+                                    <h1 class="mt-1">You haven't added anything yet.</h1>
+                                    <p class="text-muted">Shop is a simple and effective way to offer something to your audience. It could be anything. See some examples here, here, and here</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-<?php include '../../includes/admin/admin-footer.php'; ?>
+            <?php include '../../includes/admin/admin-footer.php'; ?>
