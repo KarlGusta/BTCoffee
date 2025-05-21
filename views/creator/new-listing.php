1f2ce1267a1fce276a8c9b1071694851adeb9edf
Esimit Karlgusta <annoh.karlgusta@gmail.com>
2025-05-21 12:38:34 +0300

    Added correct link

--- views/creator/new-listing.php
+++ views/creator/new-listing.php
@@ -37,7 +37,7 @@ include_once '../../config/paths.php';
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <h1 class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
-                    <a href="https://github.com/tabler/tabler" class="btn btn-light btn-icon w-100" target="_blank" rel="noreferrer">
+                    <a href="extras.php" class="btn btn-light btn-icon w-100" target="_blank" rel="noreferrer">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-left">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                             <path d="M15 6l-6 6l6 6" />
